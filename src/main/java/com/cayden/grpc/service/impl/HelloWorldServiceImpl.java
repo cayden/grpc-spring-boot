e2f6db2408461ba4188ec448ea14d75252e8479f
cuiran <cuiran2001@163.com>
2020-06-28 22:11:27 +0800

    update log

--- src/main/java/com/cayden/grpc/service/impl/HelloWorldServiceImpl.java
+++ src/main/java/com/cayden/grpc/service/impl/HelloWorldServiceImpl.java
@@ -4,6 +4,7 @@ import com.cayden.grpc.helloworld.Greeting;
 import com.cayden.grpc.helloworld.HelloWorldServiceGrpc;
 import com.cayden.grpc.helloworld.Person;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import org.lognet.springboot.grpc.GRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,23 +12,22 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by cuiran on 20/6/28.
  */
+@Slf4j
 @GRpcService
 public class HelloWorldServiceImpl  extends HelloWorldServiceGrpc.HelloWorldServiceImplBase{
 
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(HelloWorldServiceImpl.class);
 
     @Override
     public void sayHello(Person request,
                          StreamObserver<Greeting> responseObserver) {
-        LOGGER.info("server received {}", request);
+        log.info("server received {}", request);
 
         String message = "Hello " + request.getFirstName() + " "
                 + request.getLastName() + "!";
         Greeting greeting =
                 Greeting.newBuilder().setMessage(message).build();
-        LOGGER.info("server responded {}", greeting);
+        log.info("server responded {}", greeting);
 
         responseObserver.onNext(greeting);
         responseObserver.onCompleted();
